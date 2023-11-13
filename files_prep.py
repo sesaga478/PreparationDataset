b1ffc8d77a497a6d9380e99e54257b1a74874314
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-11-13 10:38:35 -0500

    Update files_prep.py

--- files_prep.py
+++ files_prep.py
@@ -5,5 +5,5 @@ def rename_files_folder(folder,ext1,ext2):
     infilename = os.path.join(folder,filename)
     if not os.path.isfile(infilename): continue
     oldbase = os.path.splitext(filename)
-    newname = infilename.replace(ext, '')
+    newname = infilename.replace(ext1, ext2)
     output = os.rename(infilename, newname)
