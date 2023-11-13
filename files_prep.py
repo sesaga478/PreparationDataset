93645220bd1438db64835b8a328398d0def88466
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-11-13 07:23:39 -0500

    Create files_prep.py

---
+++ files_prep.py
@@ -0,0 +1,9 @@
+import os,sys
+
+def rename_files_folder(folder,ext1,ext2):
+  for filename in os.listdir(folder):
+    infilename = os.path.join(folder,filename)
+    if not os.path.isfile(infilename): continue
+    oldbase = os.path.splitext(filename)
+    newname = infilename.replace(ext, '')
+    output = os.rename(infilename, newname)
